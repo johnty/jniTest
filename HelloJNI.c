dd0ec702f80fec6f747efc9c6057c0c54b8dac5c
johnty <johnty@infusionsystems.com>
2014-11-06 14:12:19 -0500

    fixed C jni function names with packagename

--- HelloJNI.c
+++ HelloJNI.c
@@ -2,12 +2,16 @@
 #include <stdio.h>
 #include "testjni_HelloJNI.h"
 
-JNIEXPORT void JNICALL Java_HelloJNI_sayHello(JNIEnv *env, jobject thiz) {
+JNIEXPORT void JNICALL Java_testjni_HelloJNI_sayHello(JNIEnv *env, jobject thiz) {
    printf("Hello World!\n");
    return;
 }
 
-JNIEXPORT void JNICALL Java_HelloJNI_setDouble (JNIEnv *env, jobject thiz, jdouble val) {
+JNIEXPORT void JNICALL Java_testjni_HelloJNI_setDouble (JNIEnv *env, jobject thiz, jdouble val) {
    printf("do stuff here...\n");
    return;
+}
+
+JNIEXPORT jdouble JNICALL Java_testjni_HelloJNI_average (JNIEnv *env, jobject thiz, jint n1, jint n2) {
+   return n1+n2;
 }
--- testjni/HelloJNI.java
+++ testjni/HelloJNI.java
@@ -11,6 +11,7 @@ public class HelloJNI {
    private native void sayHello();
    private native double getDouble();
    private native void setDouble(double d);
+   private native double average(int n1, int n2);
 
    public native byte[] getByteArray();
 
@@ -19,5 +20,8 @@ public class HelloJNI {
    // Test Driver
    public static void main(String[] args) {
       new HelloJNI().sayHello();  // invoke the native method
+      System.out.println("Testing JNI C function: average of 3 and 4 is: ");
+      double res = new HelloJNI().average(3,4);
+      System.out.println("..." + res);
    }
 }
